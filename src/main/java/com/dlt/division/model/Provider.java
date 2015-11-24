f219b924773865dd1b35fc034bbc594d0430d6ad
Rick Stewart <rick.stewart@dlt.com>
2015-11-24 17:44:51 -0500

    Adding Provider services

---
+++ src/main/java/com/dlt/division/model/Provider.java
@@ -0,0 +1,47 @@
+package com.dlt.division.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@Table(name="provider")
+public class Provider {
+
+    @Id
+    @Column(name = "id")
+    int id;
+    
+    @Column(name = "name")
+    String name;
+    
+    @Column(name = "website_url")
+    String website_url;
+
+    public int getId() {
+      return id;
+    }
+
+    public void setId(int id) {
+      this.id = id;
+    }
+
+    public String getName() {
+      return this.name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getWebSiteUrl() {
+      return this.website_url;
+    }
+
+    public void setWebSiteUrl(String sUrl) {
+      this.website_url = sUrl;
+    }
+}
