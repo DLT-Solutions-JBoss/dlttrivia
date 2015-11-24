5fa9da38ce012bac4cdcce0f896df3757ae588ea
Rick Stewart <rick.stewart@dlt.com>
2015-11-24 17:17:53 -0500

    adding scheduling actions

---
+++ src/main/java/com/dlt/division/model/Schedule.java
@@ -0,0 +1,49 @@
+package com.dlt.division.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@Table(name="schedule")
+public class Schedule {
+
+    @Id
+    @Column(name = "id")
+    int id;
+    
+    @Column(name = "emp_id")
+    int emp_id;
+    
+    @Column(name = "delivery_date")
+    Date delivery_date;
+
+    public int getId() {
+      return id;
+    }
+
+    public void setId(int id) {
+      this.id = id;
+    }
+
+    public int getEmployeeId() {
+      return this.emp_id;
+    }
+
+    public void setEmployeeId(int empId) {
+      this.emp_id = empId;
+    }
+
+    public Date getDeliveryDate() {
+      return this.delivery_date;
+    }
+
+    public void setDeliveryDate(Date dDate) {
+      this.delivery_date = dDate;
+    }
+}
---
+++ src/main/java/com/dlt/division/rest/ScheduleService.java
@@ -0,0 +1,68 @@
+package com.dlt.division.rest;
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
+
+import com.dlt.division.model.Schedule;
+
+
+//Dummy comment
+@Path("/EP")
+public class ScheduleService implements DivisionService{
+
+
+
+        @DivisionService(ServiceType.EP)
+        @PersistenceContext(unitName="Division", type=PersistenceContextType.EXTENDED)
+        private EntityManager emSchedule;
+
+
+
+        @GET()
+        @Path("schedule/{ScheduleId}")
+        @Produces("application/json")
+        @DivisionService(ServiceType.EP)
+        public List<Schedule> getSchedule(@PathParam("ScheduleId") String sScheduleId)
+        {
+
+                Query query = emSchedule.createQuery("FROM com.dlt.division.model.Schedule where id = ?1");
+                query.setParameter(1,sScheduleId);
+                @SuppressWarnings("unchecked")
+                List <Schedule> Schedule = query.getResultList();
+
+                return Schedule;
+        }
+
+        @GET()
+        @Path("schedules")
+        @Produces("application/json")
+        @DivisionService(ServiceType.EP)
+        public List<Schedule> getSchedules()
+        {
+
+                Query query = emSchedule.createQuery("FROM com.dlt.division.model.Schedule order by delivery_date");
+                @SuppressWarnings("unchecked")
+                List <Schedule> Schedule = query.getResultList();
+
+                return Schedule;
+        }
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+                return null;
+        }
+        @Override
+        public ServiceType value() {
+                return null;
+        }
+}
+
