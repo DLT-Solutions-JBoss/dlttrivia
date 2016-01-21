aae354af01952e2d4bf068b1cdb4bebe0d6af9d7
Rick Stewart <rick.stewart@dlt.com>
2016-01-21 17:28:58 -0500

    getting joins right

--- src/main/java/com/dlt/division/model/Schedule.java
+++ src/main/java/com/dlt/division/model/Schedule.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -27,6 +28,7 @@ public class Schedule {
     Date delivery_date;
 
     @ManyToOne
+    @JoinColumn(name="emp_id")
     private Employee employee;
 
     public int getId() {
