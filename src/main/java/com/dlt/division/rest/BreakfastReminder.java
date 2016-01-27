f7c913e4f63f7708a0c1d99980c2e0c81b8be1ba
Rick Stewart <rick.stewart@dlt.com>
2016-01-27 15:24:30 -0500

    Adding email reminder

---
+++ src/main/java/com/dlt/division/rest/BreakfastReminder.java
@@ -0,0 +1,99 @@
+package com.dlt.division.rest;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.dlt.division.model.Schedule;
+
+//Dummy comment
+@Path("/EP")
+public class BreakfastReminder implements DivisionService{
+
+
+
+        @DivisionService(ServiceType.EP)
+        @PersistenceContext(unitName="Division", type=PersistenceContextType.EXTENDED)
+        private EntityManager emSchedule;
+
+        @GET()
+        @Path("email")
+        @Produces("application/json")
+        @DivisionService(ServiceType.EP)
+        public List<Schedule> sendEmail()
+        {
+
+                Query query = emSchedule.createQuery("FROM com.dlt.division.model.Schedule where delivery_date > now() limit 1");
+                @SuppressWarnings("unchecked")
+                List <Schedule> Schedule = query.getResultList();
+                
+                if(!Schedule.isEmpty())
+                {
+                	//Email scheduled employee
+            		final String username = "DLT.JBoss@gmail.com";
+            		final String password = "DLTJBossMW";
+
+            		Properties props = new Properties();
+            		props.put("mail.smtp.auth", "true");
+            		props.put("mail.smtp.starttls.enable", "true");
+            		props.put("mail.smtp.host", "smtp.gmail.com");
+            		props.put("mail.smtp.port", "587");
+            		
+            		Session session = Session.getInstance(props,
+            				  new javax.mail.Authenticator() {
+            					protected PasswordAuthentication getPasswordAuthentication() {
+            						return new PasswordAuthentication(username, password);
+            					}
+            				  });
+            		
+            		try {
+
+            			Message message = new MimeMessage(session);
+            			message.setFrom(new InternetAddress(username));
+            			message.setRecipients(Message.RecipientType.TO,
+            				InternetAddress.parse("rickstewart0417@gmail.com"));
+            			message.setSubject("Testing Subject");
+            			message.setText("Dear Mail Crawler,"
+            				+ "\n\n No spam to my email, please!");
+
+            			Transport.send(message);
+
+            			System.out.println("Done");
+
+            		} catch (MessagingException e) {
+            			throw new RuntimeException(e);
+            		}	
+            		
+                }
+
+                return Schedule;
+        }
+
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+                return null;
+        }
+        @Override
+        public ServiceType value() {
+                return null;
+        }
+}
+
