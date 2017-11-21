f21a3bc40fd8c27b72854f5e444619a2cfbec3f9
Rick Stewart <rick.stewart@dlt.com>
2017-11-21 17:39:03 -0500

    added ask and response classes

---
+++ src/main/java/com/dlt/division/model/Ask.java
@@ -0,0 +1,90 @@
+package com.dlt.division.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.sql.Date;
+
+@XmlRootElement
+@Entity
+@Table(name="ask")
+public class Ask {
+
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "ask_id")
+    int ask_id;
+
+    @ManyToOne
+    @JoinColumn(name="scheduled_question_id")
+    private ScheduledQuestion scheduledQuestion;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(name = "asked", columnDefinition="DATETIME"))
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date asked;
+
+    @Column(name = "created", columnDefinition="DATETIME"))
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Column(name = "updated", columnDefinition="DATETIME"))
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated;
+
+    public int getAskId() {
+      return ask_id;
+    }
+
+    public void setAskId(int ask_id) {
+      this.ask_id = ask_id;
+   }
+
+    public ScheduledQuestion getScheduledQuestion() {
+      return scheduledQuestion;
+    }
+
+    public void setScheduledQuestion(ScheduledQuestion question) {
+      this.scheduledQuestion = question;
+    }
+
+   public Date getAsked() {
+      return asked;
+    }
+
+    public void setAsked(Date asked) {
+      this.asked = asked;
+   }
+
+    public User getUser() {
+      return user;
+    }
+
+    public void setUser(User user) {
+      this.user = user;
+   }
+
+    public Date getCreated() {
+      return this.created;
+    }
+
+    public void setCreated(Date created) {
+      this.created = created;
+    }
+
+    public Date getUpdated() {
+      return this.updated;
+    }
+
+    public void setUpdated(Date updated) {
+      this.updated = updated;
+    }
+
+}
---
+++ src/main/java/com/dlt/division/model/Response.java
@@ -0,0 +1,90 @@
+package com.dlt.division.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.sql.Date;
+
+@XmlRootElement
+@Entity
+@Table(name="response")
+public class Response {
+
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "response_id")
+    int response_id;
+
+    @ManyToOne
+    @JoinColumn(name="ask_id")
+    private Ask ask;
+
+    @ManyToOne
+    @JoinColumn(name="choice_id")
+    private Choice choice;
+
+    @Column(name = "responded", columnDefinition="DATETIME"))
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date responded;
+
+    @Column(name = "created", columnDefinition="DATETIME"))
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Column(name = "updated", columnDefinition="DATETIME"))
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated;
+
+    public int getResponseId() {
+      return response_id;
+    }
+
+    public void setResponseId(int response_id) {
+      this.response_id = response_id;
+   }
+
+    public Ask getAsk() {
+      return ask;
+    }
+
+    public void setAsk(Ask ask) {
+      this.ask = ask;
+    }
+
+   public Date getResponded() {
+      return responded;
+    }
+
+    public void setAsked(Date responded) {
+      this.responded = responded;
+   }
+
+    public Choice getChoice() {
+      return choice;
+    }
+
+    public void setChoice(Choice choice) {
+      this.choice = choice;
+   }
+
+    public Date getCreated() {
+      return this.created;
+    }
+
+    public void setCreated(Date created) {
+      this.created = created;
+    }
+
+    public Date getUpdated() {
+      return this.updated;
+    }
+
+    public void setUpdated(Date updated) {
+      this.updated = updated;
+    }
+
+}
