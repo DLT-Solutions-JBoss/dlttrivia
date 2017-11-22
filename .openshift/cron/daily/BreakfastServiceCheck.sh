6574fe047659c507a20f17e5a32df9547520972b
Rick Stewart <rick.stewart@dlt.com>
2017-11-22 10:23:12 -0500

    removing extraneous directories

--- .openshift/cron/daily/BreakfastServiceCheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash                                                                                                                                                                                     
-CURL='/usr/bin/curl'
-RVMHTTP="http://dltepbreakfast-dltjbossmw.rhcloud.com/rest/EP/schedule/3"
-CURLARGS="-f -s -S -k"
-
-# or you can redirect it into a file:
-$CURL $CURLARGS $RVMHTTP > /tmp/breakfast.log
