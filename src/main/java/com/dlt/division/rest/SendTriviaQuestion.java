db7f013ee3c8da3c76d90fcf4269d0a547d18cbd
Rick Stewart <rick.stewart@dlt.com>
2017-11-28 16:24:28 -0500

    Added rest call to Submit

--- src/main/java/com/dlt/division/rest/SendTriviaQuestion.java
+++ src/main/java/com/dlt/division/rest/SendTriviaQuestion.java
@@ -172,7 +172,7 @@ public class SendTriviaQuestion implements DivisionService {
                     //Create dynamic string for series of variable choices
                     StringBuffer htmlQuestionChoice = new StringBuffer();
                             	
-                    htmlQuestionChoice.append("\n<tr>\n<td>\n");
+                    htmlQuestionChoice.append("\n<tr>\n<td align=\"left\">\n");
                     
                     //Loop through choices and create rows/columns of radio buttons for the user to select
                     for (int i = 0; i < QuestionChoiceList.size(); i++) 
