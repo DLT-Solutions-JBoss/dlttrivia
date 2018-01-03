e6a59f52201e1ee154c72084083b7df8e7681b77
Rick Stewart <rick.stewart@dlt.com>
2018-01-03 12:00:04 -0500

    latest backup of DLT Trivia DB at 03/01/2018 12:00:03

--- trivia_db_backup.sh
+++ trivia_db_backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+
 echo "Starting back-up of DLT EP Trivia Database (MySQL)!"
 
 # Login to OCP
@@ -12,17 +14,18 @@ oc project $OCP_TRIVIA_PROJECT
 export TRIVIA_DB_POD=`oc get pods -o jsonpath='{.items[*].metadata.name}' --selector deploymentConfig=trivia-mysql`
 
 # Forward the port to your local machine of the Trivia DB
-oc port-forward $TRIVIA_DB_POD 13306:3306
+oc port-forward $TRIVIA_DB_POD 13306:3306 &
 
 # Run mysqldump to obtain latest trivia data
 mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=13306 --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
 
 echo "Retrived back-up of DLT EP Trivia Database (MySQL)..."
+echo ls -lrt ./src/main/resources/init_data_2.sql
 
 echo "Updating DLTTrivia Git repo..."
 
 # Commit latest file
-git commit -am "latest backup of DLT Trivia DB"
+git commit -am "latest backup of DLT Trivia DB at $dt"
 
 # Push to origin
 git push --no-verify
