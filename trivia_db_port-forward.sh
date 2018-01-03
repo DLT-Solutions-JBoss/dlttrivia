0876d7fbcc5ab27f8b428b82181a15ee0c7e1c36
Rick Stewart <rick.stewart@dlt.com>
2018-01-03 15:46:36 -0500

    latest backup of DLT Trivia DB at 01/03/2018 15:46:35

--- trivia_db_port-forward.sh
+++ trivia_db_port-forward.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Forward the port to your local machine of the Trivia DB
-port=13306
-if [[ $(netstat -ltn | grep ":${port} " | wc -l) -eq "0" ]] ; 
+if [[ $(netstat -ltn | grep ":${TRIVIA_DB_LOCAL_PORT} " | wc -l) -eq "0" ]] ; 
 then 
-  oc port-forward $TRIVIA_DB_POD $port:3306 &
+  oc port-forward $TRIVIA_DB_POD $TRIVIA_DB_LOCAL_PORT:3306 &
 fi
