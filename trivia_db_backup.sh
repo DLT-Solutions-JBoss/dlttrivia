89f3e1870dac7ead2fa17681954a876fa8a02b63
Rick Stewart <rick.stewart@dlt.com>
2018-01-03 12:42:21 -0500

    latest backup of DLT Trivia DB at 01/03/2018 12:42:20

--- trivia_db_backup.sh
+++ trivia_db_backup.sh
@@ -19,7 +19,7 @@ oc port-forward $TRIVIA_DB_POD 13306:3306 &
 # Run mysqldump to obtain latest trivia data
 mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=13306 --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
 
-echo "Retrived back-up of DLT EP Trivia Database (MySQL)..."
+echo "Retrieved back-up of DLT EP Trivia Database (MySQL)..."
 ls -lrt ./src/main/resources/init_data_2.sql
 
 echo "Updating DLTTrivia Git repo..."
