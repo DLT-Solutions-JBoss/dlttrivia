076b245ff4c792bf7a5a0692d2a4822712eab72a
Rick Stewart <rick.stewart@dlt.com>
2018-01-03 13:30:11 -0500

    latest backup of DLT Trivia DB at 01/03/2018 13:30:10

--- trivia_db_backup.sh
+++ trivia_db_backup.sh
@@ -15,16 +15,20 @@ export TRIVIA_DB_POD=`oc get pods -o jsonpath='{.items[*].metadata.name}' --sele
 
 # Forward the port to your local machine of the Trivia DB
 
-port="13306"
+port=13306
 if [[ $(netstat -ltn | grep ":${port} " | wc -l) -eq "0" ]] ; 
-then oc port-forward $TRIVIA_DB_POD $port:3306; 
+then "oc port-forward $TRIVIA_DB_POD $port:3306 &"; 
 fi
 
 # Run mysqldump to obtain latest trivia data
-mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=13306 --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
+echo mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$port --no-create-info --skip-triggers --compact 
+mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$port --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
 
 echo "Retrieved back-up of DLT EP Trivia Database (MySQL)..."
 ls -lrt ./src/main/resources/init_data_2.sql
+
+# Logout of OCP
+oc logout
 
 echo "Updating DLTTrivia Git repo..."
 
