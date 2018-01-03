ddeb75cc111e07bf4f054ffe81c32cf439074f4f
Rick Stewart <rick.stewart@dlt.com>
2018-01-03 14:10:43 -0500

    latest backup of DLT Trivia DB at 01/03/2018 14:10:38

--- trivia_db_backup.sh
+++ trivia_db_backup.sh
@@ -18,13 +18,13 @@ export TRIVIA_DB_POD=`oc get pods -o jsonpath='{.items[*].metadata.name}' --sele
 port=13306
 if [[ $(netstat -ltn | grep ":${port} " | wc -l) -eq "0" ]] ; 
 then 
-  oc port-forward $TRIVIA_DB_POD $port:3306  
+  oc port-forward $TRIVIA_DB_POD $port:3306 &&
   mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$port --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
 fi
 
 # Run mysqldump to obtain latest trivia data
 echo mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$port --no-create-info --skip-triggers --compact 
-mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$port --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
+#mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$port --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
 
 echo "Retrieved back-up of DLT EP Trivia Database (MySQL)..."
 ls -lrt ./src/main/resources/init_data_2.sql
