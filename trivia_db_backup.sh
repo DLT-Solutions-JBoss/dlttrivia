7be90cd8a4d71cde295bdc8732d152a97238c4a0
Rick Stewart <rick.stewart@dlt.com>
2018-01-04 14:02:01 -0500

    changed init_data.sql

--- trivia_db_backup.sh
+++ trivia_db_backup.sh
@@ -4,10 +4,10 @@ dt=$(date '+%m/%d/%Y %H:%M:%S');
 
 # Run mysqldump to obtain latest trivia data
 echo mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$TRIVIA_DB_LOCAL_PORT --no-create-info --skip-triggers --compact 
-mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$TRIVIA_DB_LOCAL_PORT --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
+mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$TRIVIA_DB_LOCAL_PORT --no-create-info --skip-triggers --compact > ./src/main/resources/init_data.sql
 
 echo "Retrieved back-up of DLT EP Trivia Database (MySQL)..."
-ls -lrt ./src/main/resources/init_data_2.sql
+ls -lrt ./src/main/resources/init_data.sql
 
 # Logout of OCP
 oc logout
--- trivia_db_port-forward.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Forward the port to your local machine of the Trivia DB
-if [[ $(netstat -ltn | grep ":${TRIVIA_DB_LOCAL_PORT} " | wc -l) -eq "0" ]] ; 
-then 
-  oc port-forward $TRIVIA_DB_POD $TRIVIA_DB_LOCAL_PORT:3306 &
-fi
