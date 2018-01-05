38c3e21a73ed7eedea3e57e9cc89ac104b83de61
Rick Stewart <rick.stewart@dlt.com>
2018-01-05 11:30:49 -0500

    Moving backup file from deployment configuration

--- trivia_db_backup.sh
+++ trivia_db_backup.sh
@@ -4,10 +4,10 @@ dt=$(date '+%m/%d/%Y %H:%M:%S');
 
 # Run mysqldump to obtain latest trivia data
 echo mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$TRIVIA_DB_LOCAL_PORT --no-create-info --skip-triggers --compact 
-mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$TRIVIA_DB_LOCAL_PORT --no-create-info --skip-triggers --compact > ./src/main/resources/init_data.sql
+mysqldump trivia user question choice question_choice tag question_tag contest contestant scheduled_question ask response hibernate_sequence --user=$TRIVIA_DB_USER --password=$TRIVIA_DB_PWD --host=127.0.0.1 --port=$TRIVIA_DB_LOCAL_PORT --no-create-info --skip-triggers --compact > ./src/main/resources/init_data_2.sql
 
 echo "Retrieved back-up of DLT EP Trivia Database (MySQL)..."
-ls -lrt ./src/main/resources/init_data.sql
+ls -lrt ./src/main/resources/init_data_2.sql
 
 # Logout of OCP
 oc logout
